29e6e1b6385a2d18410ca327a89eac5f7b912142
jhlee <jhlee@hlabtech.com>
2021-11-11 09:48:16 +0900

    추천 영상 랜덤하게 추천

--- api/video.js
+++ api/video.js
@@ -259,7 +259,7 @@ api.get('/recommend/:userUID', verifyToken, function (req, res) {
           + "from video "
 		  + "join my_category on video.categoryUID = my_category.categoryUID "
           + "where my_category.userUID = ? and video.status = 'act' "
-          + "order by video.regDate desc "
+          + "order by rand() desc "
 		  + "limit 20";
   var data = req.params.userUID;
 
