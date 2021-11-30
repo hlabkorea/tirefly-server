8d75a6b904876fceba6f33db3b53cb46691fa27e
jhlee <jhlee@hlabtech.com>
2021-11-30 09:45:46 +0900

    관리자 계정 비밀번호 변경

--- api/admin.js
+++ api/admin.js
@@ -3,6 +3,7 @@ const sha256 = require('sha256');
 const db = require('./config/database.js');
 const jwt = require("jsonwebtoken");
 const secretObj = require("./config/jwt.js");
+const {verifyAdminToken} = require("./config/authCheck.js");
 const {verifyToken} = require("./config/authCheck.js");
 const api = express.Router();
 
@@ -20,10 +21,10 @@ api.post('/join',
 			db.query(sql, data, function (err, result) {
 				if (err) throw err;
 
-				var userUID = result.insertId;
+				var adminUID = result.insertId;
 
 				var token = jwt.sign({
-					userUID: userUID,     // 토큰의 내용(payload)
+					adminUID: adminUID,     // 토큰의 내용(payload)
 				},
 					secretObj.secret ,    // 비밀 키
 				{
@@ -31,24 +32,56 @@ api.post('/join',
 				});
 				
 				// 토큰 이력 추가
-				var token_check_sql = "select token from user_log where userUID = ? "
+				var token_check_sql = "select token from admin_log where adminUID = ? "
 									+ "order by regDate desc "
 									+ "limit 1";
 
-				db.query(token_check_sql, userUID, function (err, result, fields) {
+				db.query(token_check_sql, adminUID, function (err, result, fields) {
 					if (err) throw err;
 
 					if(token != result[0]){
-						var token_insert_sql = "insert into user_log(userUID, token) values(?, ?)";
-						var token_insert_data = [userUID, token];
+						var token_insert_sql = "insert into user_log(adminUID, token) values(?, ?)";
+						var token_insert_data = [adminUID, token];
 						db.query(token_insert_sql, token_insert_data, function (err, result) {
 							if (err) throw err;
 						});
 					}
 				});
 
-				res.status(200).json({status:200, data: {UID: userUID, token: token}, message:"success"});
+				res.status(200).json({status:200, data: {UID: adminUID, token: token}, message:"success"});
 			});
+		}
+);
+
+// 관리자 계정 비밀번호 변경
+api.put('/password',
+		verifyAdminToken,
+		function (req, res) {
+			var adminUID = req.adminUID;
+			var sql = "select password from admin where UID = ?";
+			const exec = db.query(sql, adminUID, function (err, result) {
+				if (err) throw err;
+				
+				var password = sha256(req.body.password);
+				var newPasswd = sha256(req.body.newPasswd);
+				var adminPasswd = result[0].password;
+				if(password != adminPasswd)
+					res.status(403).json({status:403, data:"false", message:"기존 비밀번호가 틀렸습니다."});
+				else if(newPasswd == adminPasswd)
+					res.status(403).json({status:403, data:"false", message:"기존 비밀번호와 동일합니다."});
+				else{
+					var update_sql = "update admin set password= ? where UID = ?";
+					var data = [newPasswd, adminUID];
+					const exec2 = db.query(update_sql, data, function (err, result) {
+						if (err) throw err;
+
+						res.status(200).json({status:200, data:"true", message:"변경되었습니다."});
+					});
+					console.log(exec2.sql);
+				}
+			});
+
+			console.log(exec.sql);
 		}
 );
 
