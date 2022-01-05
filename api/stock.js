55005e8ea1a47bed3e532c6895a7b10d40c23dcc
jhlee <jhlee@hlabtech.com>
2022-01-05 13:53:09 +0900

    엑셀 데이터 다운로드

--- api/stock.js
+++ api/stock.js
@@ -47,7 +47,7 @@ api.get('/', verifyAdminToken, async function (req, res) {
 });
 
 // cms - 엑셀 양식 다운로드
-api.get('/excel/sample', function (req, res) {
+api.get('/excel/sample', verifyAdminToken, function (req, res) {
     var wb = new xl.Workbook({
         defaultFont: {
             color: '#000000',
@@ -56,10 +56,16 @@ api.get('/excel/sample', function (req, res) {
     });
     var ws = wb.addWorksheet('Sheet 1');
 
-    var style = wb.createStyle({
+    var header_style = wb.createStyle({
         font: {
             bold: true
         },
+        alignment: {
+            horizontal: 'center'
+        }
+    });
+
+    var data_style = wb.createStyle({
         alignment: {
             horizontal: 'center'
         }
@@ -69,18 +75,22 @@ api.get('/excel/sample', function (req, res) {
     ws.column(2).setWidth(12);
     ws.row(1).setHeight(15);
 
-    ws.cell(1, 1).string('일련번호').style(style);
-    ws.cell(1, 2).string('검사일').style(style);
-    ws.cell(2,1).string('MM-21120001-02');
-    ws.cell(2,2).string('2021-12-09');
+    ws.cell(1, 1).string('일련번호').style(header_style);
+    ws.cell(1, 2).string('검사일').style(header_style);
+    ws.cell(2,1).string('MM-21120001-02').style(data_style);
+    ws.cell(2,2).string('2021-12-09').style(data_style);
 
     wb.write('stock_sample.xlsx', res);
 });
 
-api.get('/excel', function (req, res) {
-    var accUID = req.params.accUID;
-    var sql = "select * from stock";
-    db.query(sql, accUID, function (err, result) {
+api.get('/excel', verifyAdminToken, function (req, res) {
+    var sql = "select stock.UID as stockUID, stock.serialNo, stock.testDate, ifnull(payment.shipRcpnt, '-') as shipRcpnt, ifnull(payment.buyerTel, '-') as buyerTel, "
+            + "ifnull(payment.shipConfDate, '-') as shipConfDate, if(stock.paymentUID = 0, '입고', '출고') as stockState "
+            + "from stock "
+            + "left join payment on stock.paymentUID = payment.UID "
+            + "order by stock.regDate desc";
+
+    db.query(sql, function (err, result) {
         if (err) throw err;
 
         var wb = new xl.Workbook({
@@ -91,7 +101,7 @@ api.get('/excel', function (req, res) {
         });
         var ws = wb.addWorksheet('Sheet 1');
     
-        var style = wb.createStyle({
+        var header_style = wb.createStyle({
             font: {
                 bold: true
             },
@@ -99,27 +109,46 @@ api.get('/excel', function (req, res) {
                 horizontal: 'center'
             }
         });
+
+        var data_style = wb.createStyle({
+            alignment: {
+                horizontal: 'center'
+            }
+        });
     
-        ws.column(1).setWidth(20);
-        ws.column(2).setWidth(12);
+        ws.column(1).setWidth(10);
+        ws.column(2).setWidth(20);
+        ws.column(3).setWidth(12);
+        ws.column(4).setWidth(10);
+        ws.column(5).setWidth(15);
+        ws.column(6).setWidth(20); 
+        ws.column(7).setWidth(10); 
         ws.row(1).setHeight(15);
 
-        ws.cell(1, 1).string('일련번호').style(style);
-        ws.cell(1, 2).string('검사일').style(style);
-        ws.cell(1, 3).string('수령인').style(style);
-        ws.cell(1, 4).string('전화번호').style(style);
-        ws.cell(1, 5).string('출고일').style(style);
-        ws.cell(1, 6).string('재고 상태').style(style);
+        ws.cell(1, 1).string('stockUID').style(header_style);
+        ws.cell(1, 2).string('일련번호').style(header_style);
+        ws.cell(1, 3).string('검사일').style(header_style);
+        ws.cell(1, 4).string('수령인').style(header_style);
+        ws.cell(1, 5).string('전화번호').style(header_style);
+        ws.cell(1, 6).string('출고일').style(header_style);
+        ws.cell(1, 7).string('재고 상태').style(header_style);
 
-        res.status(200).json({
-            status: 200,
-            data: result,
-            message: "success"
-        });
+        console.log(result);
+        for(var i in result){
+            var row = Number(i)+2;
+            ws.cell(row,1).string(String(result[i].stockUID)).style(data_style);
+            ws.cell(row,2).string(result[i].serialNo).style(data_style);
+            ws.cell(row,3).string(result[i].testDate).style(data_style);
+            ws.cell(row,4).string(result[i].shipRcpnt).style(data_style);
+            ws.cell(row,5).string(result[i].buyerTel).style(data_style);
+            ws.cell(row,6).string(result[i].shipConfDate).style(data_style);
+            ws.cell(row,7).string(result[i].stockState).style(data_style);
+        }
+        wb.write('stock_list.xlsx', res);
     });
 });
 
-api.post('/excel', memoryUpload.single('file'), function (req, res) {
+api.post('/excel', verifyAdminToken, memoryUpload.single('stock_file'), function (req, res) {
     // 엑셀 데이터 json으로 변환
     const result = excelToJson({
         source: req.file.buffer
